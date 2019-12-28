538fe22632ff87ed8aa00e673b8b70346094d1dc
Karen V <karenvo@usc.edu>
2019-12-28 09:19:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-  return Object.assign(driver, { [key]: value });
+  // return Object.assign(driver, { [key]: value });
+  driver[key] = value;
+  return driver;
 };
 
 function deleteFromDriverByKey(driver, key) {
